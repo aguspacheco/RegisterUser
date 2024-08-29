b063a4fc85dcb9f74accf4d05249c73429636146
Adriel <montes.adriel@gmail.com>
2024-08-29 13:40:43 -0300

    terminado la validacion

--- page/static/Libs/js/form/validacion.js
+++ page/static/Libs/js/form/validacion.js
@@ -1,9 +1,38 @@
-const inputs = document.querySelectorAll('input[type="number"]');
+const formularios = [ 
+  document.getElementById('formularioAntecedente'),
+  document.getElementById('formularioPersona'),
+  document.getElementById('formularioMatricula'),
+  document.getElementById('formularioActual')
+];
+
+const inputs = document.querySelectorAll('input[type="number"], input[type="text"], input[type="date"]');
+const botonEnviar = document.getElementById('enviar-btn');
 
 inputs.forEach(input => {
   input.addEventListener('input', () => {
-    if (!/^\d+$/.test(input.value)) {
-      input.value = '';
+    if (input.value.trim() !== '') {
+      input.style.border = '';
+      input.previousElementSibling.style.color = ''; 
     }
   });
-}); 
+});
+
+botonEnviar.addEventListener('click', (e) => {
+  e.preventDefault(); 
+
+  const camposVacios = formularios.flatMap(verificarFormulario);
+
+  if (camposVacios.length > 0) {
+    alert('Por favor, complete todos los campos obligatorios.');
+    camposVacios.forEach(campo => {
+      campo.style.border = '1px solid #ef233c'; 
+      campo.previousElementSibling.style.color = '#ef233c'; 
+    });
+  } else {
+    alert('Formulario enviado correctamente');
+  }
+});
+
+function verificarFormulario(formulario) {
+  return Array.from(formulario.elements).filter(campo => campo.value.trim() === '');
+}
