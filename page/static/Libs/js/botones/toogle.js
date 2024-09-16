2150efc65355e83ba04fc3a3b479803f1119dc70
Adriel <montes.adriel@gmail.com>
2024-09-16 13:40:19 -0300

    toogle funcionando

--- page/static/Libs/js/botones/toogle.js
+++ page/static/Libs/js/botones/toogle.js
@@ -1,10 +1,15 @@
-function visualizarPass() {
+function visualizarPass(abrirOjoUrl, cerrarOjoUrl) {
     var passwordInput = document.getElementById("id_contraseña");
     var passwordType = passwordInput.getAttribute("type");
+    var toggleIcon = document.getElementById("toggle-icon");
     
     if (passwordType === "password") {
         passwordInput.setAttribute("type", "text");
+        toggleIcon.src = abrirOjoUrl;
+        toggleIcon.alt = "Abrir Ojo"
     } else {
         passwordInput.setAttribute("type", "password");
+        toggleIcon.src = cerrarOjoUrl;
+        toggleIcon.alt = "Cerrar Ojo"
     }
 }
